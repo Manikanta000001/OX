72ce2019d479090f12d1641968829404a33a257d
Manikanta000001 <manisath10th@gmail.com>
2025-08-31 11:25:32 +0530

    reconnection

--- FrontEnd/socket.js
+++ FrontEnd/socket.js
@@ -6,7 +6,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://ox-3gl4.onrender.com", {
-  transports: ['polling', 'websocket'], // force WebSocket, skip polling if you want
+  transports: ['websocket', 'polling'], // force WebSocket, skip polling if you want
   reconnection: true,        // enable auto reconnect
   reconnectionAttempts: 5,   // number of attempts
   reconnectionDelay: 1000,   // delay between attempts (ms)
